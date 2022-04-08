82dfd2b105817d6ae10b2b69a63c8baac7bf1522
amyleblanc <amymccarthy2014@gmail.com>
2022-04-07 19:31:28 -0700

    assign instance variables & update store1 name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+
+puts @store1.name
+
+@store2 = Store.find_by(id: 2)
+
+puts @store2.name
+
+@store1.update(name: "Coquitlam")
+
+puts @store1.name
