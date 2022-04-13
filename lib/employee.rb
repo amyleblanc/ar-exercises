bff2744fa72fe179b1da25a6e568871c3f6ed2c6
amyleblanc <amymccarthy2014@gmail.com>
2022-04-12 18:56:30 -0700

    add validation rules & hourly_rate error message

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,11 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
 
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 40, 
+    less_than_or_equal_to: 200,
+    message: "- Hourly rate must be between 40 and 200"
+  }
 end
