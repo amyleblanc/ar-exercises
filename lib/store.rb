c254bd1bbee1213c37ce399d54bea3e5c3d0d6ab
amyleblanc <amymccarthy2014@gmail.com>
2022-04-12 19:01:00 -0700

    create custom validation for mens/womens_apparel

--- lib/store.rb
+++ lib/store.rb
@@ -10,4 +10,12 @@ class Store < ActiveRecord::Base
     greater_than_or_equal_to: 0,
     message: "- Must be a number greater than or equal to 0"
   }
+  validate :must_carry_mens_or_womens_apparel
+
+  def must_carry_mens_or_womens_apparel
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "- Must carry either mens or womens apparel")
+      errors.add(:womens_apparel, "- Must carry either mens or womens apparel")
+    end
+  end
 end
