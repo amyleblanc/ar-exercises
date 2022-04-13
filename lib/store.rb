b2678d716ed50f8939e7b56b426930660c4bf0c7
amyleblanc <amymccarthy2014@gmail.com>
2022-04-12 18:59:29 -0700

    add name/revenue validation rules & error messages

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
+  validates :name, presence: true, length: { 
+    minimum: 3, 
+    message: "- Name must be minimum 3 characters." 
+  }
+  validates :annual_revenue, numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 0,
+    message: "- Must be a number greater than or equal to 0"
+  }
 end
